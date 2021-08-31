d33cbbfea49a95b64006e10c9802326416b6fa93
beginner1986 <beginner1986@gmail.com>
2021-08-31 21:19:14 +0200

    Card data generation implemented.

--- banking/data/Card.java
+++ banking/data/Card.java
@@ -1,11 +1,47 @@
 package banking.data;
 
+import java.util.Random;
+
 class Card {
     private String number;
     private String pin;
     private double balance;
 
     public Card() {
-        balance = 0;
+        this.number = generateCardNumber();
+        this.pin = generatePin();
+        this.balance = 0;
+    }
+
+    private String generateCardNumber() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        sb.append("400000");
+        for(int i=0; i<10; i++) {
+            char digit = (char) (random.nextInt(10) + '0');
+            sb.append(digit);
+        }
+
+        return sb.toString();
+    }
+
+    private String generatePin() {
+        Random random = new Random();
+        int pin = random.nextInt(9999);
+
+        return String.format("%04d", pin);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public double getBalance() {
+        return balance;
     }
 }
