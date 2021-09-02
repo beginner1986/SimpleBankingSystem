564dd9524d12c5a4cc892f8968465d8010d666b2
beginner1986 <beginner1986@gmail.com>
2021-09-02 16:16:59 +0200

    Card number generation using Luhn algorithm implemented.

--- banking/data/Card.java
+++ banking/data/Card.java
@@ -17,19 +17,52 @@ class Card {
         System.out.println(this.number);
         System.out.println("Your card PIN:");
         System.out.println(this.pin);
+        System.out.println();
     }
 
     private String generateCardNumber() {
         StringBuilder sb = new StringBuilder();
         Random random = new Random();
 
-        sb.append("400000");
-        for(int i=0; i<10; i++) {
-            char digit = (char) (random.nextInt(10) + '0');
+        int[] number = new int[16];
+        // Bank Identification Number
+        number[0] = 4;
+        for(int i=1; i<=5; i++)
+            number[i] = 0;
+
+        // Account identifier
+        for(int i=6; i<=14; i++)
+            number[i] = random.nextInt(10);
+
+        //checksum
+        number[15] = generateChecksum(number);
+
+        for(int i=0; i<number.length; i++) {
+            char digit = Character.forDigit(number[i], 10);
             sb.append(digit);
         }
 
         return sb.toString();
+    }
+
+    private int generateChecksum(int[] input) {
+       int[] inputCopy = input.clone();
+       for(int i=0; i<inputCopy.length; i+=2) {
+           inputCopy[i] *= 2;
+           if(inputCopy[i] > 9)
+               inputCopy[i] -= 9;
+       }
+
+       int sum = 0;
+       for(int i=0; i<inputCopy.length; i++)
+           sum += inputCopy[i];
+
+       int last = sum % 10;
+
+       if(last == 0)
+           return 0;
+       else
+           return 10 - last;
     }
 
     private String generatePin() {
