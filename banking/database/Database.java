acb76f4da32afb7ed92a099ba4465808b28f933d
beginner1986 <beginner1986@gmail.com>
2021-09-15 15:15:45 +0200

    Table creation implemented.

--- banking/database/Database.java
+++ banking/database/Database.java
@@ -4,9 +4,9 @@ import org.sqlite.SQLiteDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
-
     public Database(String url) {
         SQLiteDataSource dataSource = new SQLiteDataSource();
         dataSource.setUrl(url);
@@ -16,6 +16,20 @@ public class Database {
                 System.out.printf("Connection to %s failed!\n", url);
                 System.exit(0);
             }
+
+            createTable(connection);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createTable(Connection connection) {
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS card(" +
+                    "id INTEGER PRIMARY KEY," +
+                    "number TEXT," +
+                    "pin TEXT," +
+                    "balance INTEGER DEFAULT 0)");
         } catch(SQLException e) {
             e.printStackTrace();
         }
